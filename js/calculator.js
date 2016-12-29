e6bc99a01378fafc71bbb471e34803f427ba569c
alivemary <alivemary@gmail.com>
2016-12-29 11:24:22 +0100

    First js

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,17 @@
 ﻿$(document).ready(function () {
+    var totalString = "";
+    $('button').on("click", function () {
+        if (this.value === "AC") {
+            totalString = "";
+            $('#calcWin').val('0');
+        }
+        else {
+            totalString += this.value;
+            $('#calcWin').val(totalString);
+        }
+        console.log(totalString);
+        
+    });
     $('#loadWin').addClass('hidden');
     $('#mainWin').removeClass('hidden');
     $('#errWin').addClass('hidden');
