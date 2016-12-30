f73c4f346c6c868fa2dafc005055557aac3139e0
alivemary <alivemary@gmail.com>
2016-12-30 10:12:52 +0100

    Simple ariphmetics

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,57 @@
 ﻿$(document).ready(function () {
     var totalString = "";
+    var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    var opers = ['+', '-', '*', '/'];
+    var arrN = [];
+    var arrO = [];
+    var cur = '';
+    
+    function calculate(arr1, arr2) {
+        //var nums = str.match(/[0-9]+/g);
+        //var operators = str.match(/[\+\-\/\*]+/g);
+        //console.log(nums);
+        //console.log(operators);
+        var result = arr1[0];
+        for (var i = 0; i < arr2.length; i++) {
+            switch (arr2[i]) {
+                case '+': result += arr1[i + 1]; break;
+                case '-': result -= arr1[i + 1]; break;
+                case '*': result *= arr1[i + 1]; break;
+                case '/': result /= arr1[i + 1]; break;
+            }
+        }
+        return result;
+        
+    }
+
     $('button').on("click", function () {
+        if (numbers.indexOf(this.value) !== -1) {
+            cur += this.value;
+            $('#calcWin').val(cur);
+        }
+        if (opers.indexOf(this.value) !== -1) {
+            arrO.push(this.value);
+            if (arrN.length < arrO.length) arrN.push(Number(cur));
+            cur = "";
+            
+        }
         if (this.value === "AC") {
-            totalString = "";
+            cur = "";
+            arrN = [];
+            arrO = [];
             $('#calcWin').val('0');
         }
-        else {
-            totalString += this.value;
-            $('#calcWin').val(totalString);
+        if (this.value === '=') {
+            arrN.push(Number(cur));
+            cur = calculate(arrN, arrO);
+            $('#calcWin').val(calculate(arrN, arrO));
+            arrN = [calculate(arrN, arrO)];
+            arrO = [];
         }
+        
         console.log(totalString);
+        console.log(arrN);
+        console.log(arrO);
         
     });
     $('#loadWin').addClass('hidden');
